aa10d066a48575f94000354efe26ce9923c95ca4
Rokib <hasanrokibul413@gmail.com>
2025-12-15 13:19:49 +0600

    shifted from swiper

--- src/components/virtualTryOn/Slider.jsx
+++ src/components/virtualTryOn/Slider.jsx
@@ -1,32 +1,57 @@
-import "swiper/css";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect, useRef } from "react";
 import Test3 from "../../assets/images/products/link.jpg";
 import Test from "../../assets/images/products/profile_pic.jpg";
 import Test2 from "../../assets/images/products/test.png";
 import "../../styles/slider.css";
+const products = [
+  Test,
+  Test2,
+  Test3,
+  Test,
+  Test2,
+  Test3,
+  Test,
+  Test2,
+  Test3,
+  Test,
+  Test2,
+  Test3,
+  Test,
+  Test2,
+  Test3,
+  Test3,
+  Test,
+  Test2,
+  Test3,
+];
+
 export default function Slider() {
+  const items = [...products, ...products, ...products];
+  const sliderRef = useRef(null);
+
+  useEffect(() => {
+    const el = sliderRef.current;
+    el.scrollLeft = el.scrollWidth / 3;
+  }, []);
+
+  const onScroll = () => {
+    const el = sliderRef.current;
+    const third = el.scrollWidth / 3;
+
+    if (el.scrollLeft < third * 0.5) {
+      el.scrollLeft += third;
+    } else if (el.scrollLeft > third * 1.5) {
+      el.scrollLeft -= third;
+    }
+  };
+
   return (
-    <Swiper
-      spaceBetween={2}
-      slidesPerView={3}
-      centeredSlides={true}
-      loop={true}
-      coverflowEffect={true}
-      autoplay={true}
-      grabCursor={true}
-    >
-      <SwiperSlide>
-        <img src={Test} alt="Test" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={Test2} alt="Test" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={Test3} alt="Test" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={Test} alt="Test" />
-      </SwiperSlide>
-    </Swiper>
+    <div ref={sliderRef} onScroll={onScroll} className="slider">
+      {items.map((p, i) => (
+        <div className="slide" key={i}>
+          <img src={p} />
+        </div>
+      ))}
+    </div>
   );
 }
--- src/pages/TryOn.jsx
+++ src/pages/TryOn.jsx
@@ -3,9 +3,9 @@ import Slider from "../components/virtualTryOn/Slider";
 
 export default function TryOn() {
   return (
-    <div className="relative   mx-auto my-2 max-h-screen overflow-hidden">
+    <div className="relative mx-auto max-h-screen overflow-hidden">
       <MyCam />
-      <div className="w-full flex justify-center items-center">
+      <div className="absolute bottom-2 w-full h-50">
         <Slider />
       </div>
     </div>
